062377995afcdfacef9555926f8ce65f02c02fa3
AdolfoZepeda <adolfitozepeda@gmail.com>
2024-10-03 18:36:50 -0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+//swiper
+
+var swiper = new Swiper(".home", {
+
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+
+});
